0e05c26c8ad2c975cd6b0870a5c4fb57417644bc
Kyle Stanfield <stanfield@ucsb.edu>
2020-01-25 18:32:01 -0800

    fixed buttons by reAdding slide change functions

--- aidan/settings.py
+++ aidan/settings.py
@@ -118,7 +118,7 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.0/howto/static-files/
 
-STATIC_ROOT = ''
+STATIC_ROOT = 'myStatic'
 
 STATIC_URL = '/static/'
 
